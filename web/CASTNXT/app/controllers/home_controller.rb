71ddd9cbff365c1ae001758cef4365c0122b54be
EC2 Default User <ec2-user@ip-172-31-87-204.ec2.internal>
2022-03-15 01:49:58 +0000

    DB + Login Controller

--- web/CASTNXT/app/controllers/home_controller.rb
+++ web/CASTNXT/app/controllers/home_controller.rb
@@ -1,4 +1,58 @@
 class HomeController < ApplicationController
+
   def index
+    if session.key?(:userEmail) and session.key?(:userType) and session.key?(:userName)
+      session_redirect
+    end
+    
+    if params.key?(:email) and params.key?(:password)
+      if correct_user?(params)
+      currentUser = User.find_by(:email => params[:email], :password => params[:password])
+      session[:userEmail] = params[:email]
+      session[:userType] = currentUser.userType
+      session[:userName] = currentUser.name
+      session_redirect
+      else
+        render json: {comment: "User not found!"}, status: 400
+      end
+    end
+    
+  end
+  
+  def create
+    if new_user?(params[:email])
+      create_user(params)
+      session[:userEmail] = params[:email]
+      session[:userType] = params[:type]
+      session_redirect
+    else
+      render json: {comment: "Email already exists!"}, status: 400
+    end
+  end
+  
+  private
+  
+  def new_user? email
+    if User.where(:email => email).blank?
+      return true
+    end
+    
+    return false
+  end
+  
+  def correct_user? params
+    if User.where(:email => params[:email], :password => params[:password]).present?
+      return true
+    end
+    
+    return false
+  end
+  
+  def create_user params
+    User.create(name:params[:name], email:params[:email], password:params[:password], userType:params[:type])
+  end
+
+  def session_redirect
+    render json: {userType: session[:userType]}, status: 200
   end
 end
