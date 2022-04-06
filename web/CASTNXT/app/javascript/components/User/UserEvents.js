23f625d419174d38fb39227c5656dd2ee7be61c7
EC2 Default User <ec2-user@ip-172-31-54-152.ec2.internal>
2022-04-06 21:38:28 +0000

    sync up

---
+++ web/CASTNXT/app/javascript/components/User/UserEvents.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+
+class UserEvents extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        
+        let trows = [
+            {
+                event: 'Event 1',
+                link: 'Link 1',
+                status: 'Under Consideration'
+            },
+            {
+                event: 'Event 2',
+                link: 'Link 2',
+                status: 'Under Consideration'
+            },
+            {
+                event: 'Event 3',
+                link: 'Link 3',
+                status: 'Under Consideration'
+            },
+        ]
+        
+        return(
+            <div className="container user-events">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Event</td>
+                                    <td>Link</td>
+                                    <td>Status</td>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                {trows.map((val, key) => {
+                                    return(
+                                        <tr key={key}>
+                                            <td>{val.event}</td>
+                                            <td>{val.link}</td>
+                                            <td>{val.status}</td>
+                                        </tr>
+                                    )
+                                }
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserEvents
