99ae0123038fd93af50e48549537cb7fb386b8c0
EC2 Default User <ec2-user@ip-172-31-54-152.ec2.internal>
2022-05-03 16:16:54 +0000

    added update negotiation feature for client

--- web/CASTNXT/app/controllers/negotiations_controller.rb
+++ web/CASTNXT/app/controllers/negotiations_controller.rb
@@ -16,7 +16,22 @@ class NegotiationsController < ApplicationController
   
   # PUT
   def update
+    Rails.logger.debug("Inside negotiation/update index")
+    Rails.logger.debug(params)
+    existingNegotiation = Negotiation.find_by(:event_id => params["event_id"], :client_id => params["client_id"])
+    Rails.logger.debug(existingNegotiation)
     
+    # update existing negotiation wrt changes in intermediate and final slides for a client
+    existingNegotiation.update(:intermediateSlides => params["intermediateSlides"], :finalSlides => params["finalSlides"])
+  
+    # For any new updates in finalSlides, update the respective talent's form submission as "ACCEPTED"
+    params["finalSlides"].each do |slideId|
+      slide = Slide.find_by(:_id => slideId)
+      unless "ACCEPTED".casecmp? slide.submission_status
+        slide.update(:submission_status => 'ACCEPTED')
+      end
+    end
+    render json: {comments: 'Updated negotiation object'}, status: 200
   end
 
   # POST /negotiations
