07a8c8d1c424eee4406bc6d8679961845d34ef99
EC2 Default User <ec2-user@ip-172-31-54-152.ec2.internal>
2022-05-03 16:28:24 +0000

    update negotiation changes to supprt admin/client

--- web/CASTNXT/app/controllers/negotiations_controller.rb
+++ web/CASTNXT/app/controllers/negotiations_controller.rb
@@ -18,20 +18,29 @@ class NegotiationsController < ApplicationController
   def update
     Rails.logger.debug("Inside negotiation/update index")
     Rails.logger.debug(params)
-    existingNegotiation = Negotiation.find_by(:event_id => params["event_id"], :client_id => params["client_id"])
-    Rails.logger.debug(existingNegotiation)
     
-    # update existing negotiation wrt changes in intermediate and final slides for a client
-    existingNegotiation.update(:intermediateSlides => params["intermediateSlides"], :finalSlides => params["finalSlides"])
-  
-    # For any new updates in finalSlides, update the respective talent's form submission as "ACCEPTED"
-    params["finalSlides"].each do |slideId|
-      slide = Slide.find_by(:_id => slideId)
-      unless "ACCEPTED".casecmp? slide.submission_status
-        slide.update(:submission_status => 'ACCEPTED')
+    if is_user_logged_in?('ADMIN')
+      existingNegotiation = Negotiation.find_by(:event_id => params["event_id"], :client_id => params["client_id"])
+      Rails.logger.debug(existingNegotiation)
+      # update existing negotiation wrt changes in intermediate and final slides for a client
+      existingNegotiation.update(:intermediateSlides => params["intermediateSlides"], :finalSlides => params["finalSlides"])
+      # For any new updates in finalSlides, update the respective talent's form submission as "ACCEPTED"
+      params["finalSlides"].each do |slideId|
+        slide = Slide.find_by(:_id => slideId)
+        unless "ACCEPTED".casecmp? slide.submission_status
+          slide.update(:submission_status => 'ACCEPTED')
+        end
       end
+      render json: {comments: 'Updated negotiation object wrt finalSlides'}, status: 200
+    elsif is_user_logged_in?('CLIENT')
+      existingNegotiation = Negotiation.find_by(:event_id => params["event_id"], :client_id => params["client_id"])
+      Rails.logger.debug(existingNegotiation)
+      # update existing negotiation wrt changes in intermediate and final slides for a client
+      existingNegotiation.update(:intermediateSlides => params["intermediateSlides"])
+      render json: {comments: 'Updated negotiation object wrt intermediateSlides'}, status: 200
+    else
+      render json: {redirect_path: '/'}, status: 403
     end
-    render json: {comments: 'Updated negotiation object'}, status: 200
   end
 
   # POST /negotiations
